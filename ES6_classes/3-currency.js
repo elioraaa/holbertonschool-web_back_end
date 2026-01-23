391338cbbf54b436693d4fded16e4460391625f8
elioraaa <elioraqendro2021@gmail.com>
2026-01-23 20:00:16 +0100

    fix 3-main

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,34 +1,22 @@
 export default class Currency {
     constructor(code, name) {
-        if (typeof code !== 'string') {
-            throw new TypeError('code must be a string');
-        }
-        if (typeof name !== 'string') {
-            throw new TypeError('name must be a string');
-        }
+        if (typeof name !== 'string') throw new TypeError('name must be a string');
+        if (typeof code !== 'string') throw new TypeError('code must be a string');
 
-        this._code = code;
         this._name = name;
+        this._code = code;
     }
 
-    get code() {
-        return this._code;
-    }
-    set code(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('code must be string');
-        }
-        this._code = value;
-    }
-
-    get name() {
-        return this._code;
-    }
+    get name() { return this._name; }
     set name(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('name must be string');
-        }
+        if (typeof value !== 'string') throw new TypeError('name must be a string');
         this._name = value;
+    }
+
+    get code() { return this._code; }
+    set code(value) {
+        if (typeof value !== 'string') throw new TypeError('code must be a string');
+        this._code = value;
     }
 
     displayFullCurrency() {
