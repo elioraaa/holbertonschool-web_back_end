62d190a413cd590a6eb47d39279ab0945bdeabcd
elioraaa <elioraqendro2021@gmail.com>
2026-01-25 18:46:04 +0100

    fixed 9-main.js

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -16,7 +16,6 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -25,7 +24,7 @@ export class StudentHolberton {
   }
 
   get fullName() {
-    return `${this._lastName} ${this._lastName}`;
+    return `${this._firstName} ${this._lastName}`;
   }
 
   get holbertonClass() {
@@ -37,11 +36,10 @@ export class StudentHolberton {
   }
 }
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
-
-export const listOfStudents = [student1, student2, student3, student4, student5];
+export const listOfStudents = [
+  new StudentHolberton('Guillaume', 'Salva', class2020),
+  new StudentHolberton('John', 'Doe', class2020),
+  new StudentHolberton('Albert', 'Clinton', class2019),
+  new StudentHolberton('Donald', 'Bush', class2019),
+  new StudentHolberton('Jason', 'Sandler', class2019),
+];
