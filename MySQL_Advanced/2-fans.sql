72b972e8eeafe6f3d285f5624598664899ebdedf
elioraaa <elioraqendro2021@gmail.com>
2026-02-27 23:37:37 +0100

    fixed 2

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,2 @@
 -- script to list the number of fans
-SELECT origin, SUM(fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
