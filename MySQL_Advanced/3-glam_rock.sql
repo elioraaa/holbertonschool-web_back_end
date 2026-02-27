20cc138eec287d834bf7e30223d04d16c85c74b1
elioraaa <elioraqendro2021@gmail.com>
2026-02-27 23:59:45 +0100

    fixed 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,23 @@
--- script to list the lifespan of glam rock bands
+-- script
 
-SELECT band_name, (CASE WHEN split IS NULL THEN 2024 ELSE split END) - formed AS lifespan
-FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY lifespan DESC;
+SET @style_col := (
+  SELECT COLUMN_NAME
+  FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'metal_bands'
+    AND COLUMN_NAME IN ('main_style', 'style')
+  ORDER BY FIELD(COLUMN_NAME, 'main_style', 'style')
+  LIMIT 1
+);
+
+SET @sql := CONCAT(
+  "SELECT band_name, ",
+  "((CASE WHEN split IS NULL OR split = 0 THEN 2024 ELSE split END) - formed) AS lifespan ",
+  "FROM metal_bands ",
+  "WHERE ", @style_col, " = 'Glam rock' ",
+  "ORDER BY lifespan DESC;"
+);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
