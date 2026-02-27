30277d7670e83cf8b09dc5488e125de05eb2ab02
elioraaa <elioraqendro2021@gmail.com>
2026-02-27 23:40:37 +0100

    fixed 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 
 SELECT
     band_name,
-    (IFNULL(split, 2024) - formed) AS lifespan
+    IFNULL(split, 2024) - formed AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC, band_name ASC;
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC;
