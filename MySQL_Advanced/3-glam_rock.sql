e02bc71e045f3a27bf6b8f6f9a747be2fda52202
elioraaa <elioraqendro2021@gmail.com>
2026-02-28 00:01:23 +0100

    fixed 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,23 +1,8 @@
 -- script
 
-SET @style_col := (
-  SELECT COLUMN_NAME
-  FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = 'metal_bands'
-    AND COLUMN_NAME IN ('main_style', 'style')
-  ORDER BY FIELD(COLUMN_NAME, 'main_style', 'style')
-  LIMIT 1
-);
-
-SET @sql := CONCAT(
-  "SELECT band_name, ",
-  "((CASE WHEN split IS NULL OR split = 0 THEN 2024 ELSE split END) - formed) AS lifespan ",
-  "FROM metal_bands ",
-  "WHERE ", @style_col, " = 'Glam rock' ",
-  "ORDER BY lifespan DESC;"
-);
-
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+SELECT
+  band_name,
+  (CASE WHEN split IS NULL OR split = 0 THEN 2024 ELSE split END) - formed AS lifespan
+FROM metal_bands
+WHERE TRIM(SUBSTRING_INDEX(style, ',', 1)) = 'Glam rock'
+ORDER BY lifespan DESC;
