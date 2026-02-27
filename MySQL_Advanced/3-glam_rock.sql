abbfcfae0d7117c181607c84eafeb58280fa879d
elioraaa <elioraqendro2021@gmail.com>
2026-02-27 23:55:43 +0100

    fixed 3

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,6 @@
 -- script to list the lifespan of glam rock bands
 
-SELECT
-  band_name,
-  IFNULL(split, 2022) - formed AS lifespan
+SELECT band_name, IFNULL(split, 2024) - formed AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
